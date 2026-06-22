e715d00dec733de04dbed268a842a247dd77e4fd
niva-sah <niva.sah654@gmail.com>
2026-06-22 13:16:22 +0545

    added js files

---
+++ app/static/css/chat.js
@@ -0,0 +1,11 @@
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target){
+            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
+
